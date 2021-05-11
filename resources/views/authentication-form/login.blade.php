658a1696917f8d98d5efde7fb338a3bc6e817d3a
yohanesrscvt <yohanesrajabasasitorus@hotmail.com>
2021-05-12 00:04:53 +0700

    bikin authentication

--- resources/views/authentication-form/login.blade.php
+++ resources/views/authentication-form/login.blade.php
@@ -1,1 +1,20 @@
-<h1>Login Form</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <label for="email">Email</label><br>
+        <input type="email" name="email" id="email" required>
+        <br><br>
+        <label for="password">Password</label><br>
+        <input type="password" name="password" id="password" required>
+        <br><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
--- resources/views/authentication-form/register.blade.php
+++ resources/views/authentication-form/register.blade.php
@@ -1,1 +1,26 @@
-<h1>Register Form</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <label for="nama">Nama</label><br>
+        <input type="text" name="nama" id="nama" required>
+        <br><br>
+        <label for="email">Email</label><br>
+        <input type="email" name="email" id="email" required>
+        <br><br>
+        <label for="password">Password</label><br>
+        <input type="password" name="password" id="password" required>
+        <br><br>
+        <label for="confirm-password">Confirm Password</label><br>
+        <input type="password" name="confirm-password" id="confirm-password" required>
+        <br><br>
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
