d34186ea35d696518d72628b08f5fd4d1428cdeb
yohanesrscvt <yohanesrajabasasitorus@hotmail.com>
2021-05-11 12:05:53 +0700

    bikin table pakai migration laravel

---
+++ database/migrations/2021_05_11_042341_create_agents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAgentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agents', function (Blueprint $table) {
+            $table->string('AgentID');
+            $table->string('AgentNama');
+            $table->string('AgentEmail');
+            $table->string('AgentPassword');
+            $table->string('AgentPicturePath');
+            $table->primary('AgentID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agents');
+    }
+}
