8100c3f1783e3a845b0471ae22d284cb5fa5df53
yohanesrscvt <yohanesrajabasasitorus@hotmail.com>
2021-05-16 13:05:49 +0700

    benerin middleware1

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,7 +54,6 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
-        'IsLogged' => \App\Http\Middleware\IsLogged::class,
         'AuthenticationValidation' => \App\Http\Middleware\AuthenticationValidation::class,// custom middleware
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
