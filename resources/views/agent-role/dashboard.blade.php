89d41f44749e9c66466f51d43365d7374477341d
yohanesrscvt <yohanesrajabasasitorus@hotmail.com>
2021-05-17 11:40:47 +0700

    change role button

--- resources/views/agent-role/dashboard.blade.php
+++ resources/views/agent-role/dashboard.blade.php
@@ -1,2 +1,3 @@
 <h1>This is Agent Dashboard</h1>
 <a href="/logout">Logout</a>
+<a href="#">Click Here to Logged as Customer</a>
--- resources/views/customer-role/dashboard.blade.php
+++ resources/views/customer-role/dashboard.blade.php
@@ -1,2 +1,3 @@
 <h1>This is Customer Dashboard</h1>
 <a href="/logout">Logout</a>
+<a href="#">Click Here to Logged in as Agent</a>
