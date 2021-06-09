1d8d7724c6fcde238a5d895311be2e8d01f843f4
yohanesrscvt <yohanesrajabasasitorus@hotmail.com>
2021-06-09 11:02:06 +0700

    bikin dashboard cara baru

--- app/Http/Controllers/ChangeRoleController.php
+++ app/Http/Controllers/ChangeRoleController.php
@@ -8,6 +8,8 @@ class ChangeRoleController extends Controller
 {
     public function SetRoleToCustomer(){
         if(session()->has('LoginID') && session()->has('UserRole') && session()->get('UserRole') == 'Agent') {
+            session()->pull('UserRole');
+            session()->flush();
             session(['UserRole' => 'Customer']);
             return redirect('/dashboard/customer');
         }
@@ -15,6 +17,8 @@ class ChangeRoleController extends Controller
 
     public function SetRoleToAgent(){
         if(session()->has('LoginID') && session()->has('UserRole') && session()->get('UserRole') == 'Customer') {
+            session()->pull('UserRole');
+            session()->flush();
             session(['UserRole' => 'Agent']);
             return redirect('/dashboard/agent');
         }
