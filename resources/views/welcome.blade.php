aa7073bf517d1723e10c39239a5fb9b95f00d5d6
yohanesrscvt <yohanesrajabasasitorus@hotmail.com>
2021-06-19 11:23:40 +0700

    styling yg sudah ada + styling welcome screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,27 @@
+@extends('master')
+@section('title','Selamat Datang di SewaBajuPedia')
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
-    <script>
-        alert(
-            'Dear User,\nWe want to inform you that as much as possible, avoid using back or forward button on the browser because there still a few bug when you using them. Thank you for using our services :)\n\nRegards,\nSewaBajuPedia Software Development Team '
-        );
-    </script>
+    <link rel="stylesheet" href="{{asset('/css/welcome.css')}}">
 </head>
 <body>
-    <a href="/login">Login</a>
-    <a href="/register">Register</a>
+    <!-- content -->
+    @section('content')
+    <div class="box">
+        <div class="block"></div>
+        <img src="{{asset('/storage/pexels-tembela-bohle-1884581.jpg')}}" alt="">
+        <div class="box-text">
+            <p class="par" style="font-size: 30px;">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam dicta impedit tempore. Iure vel omnis perferendis nihil dolor voluptate neque nulla exercitationem? Aliquid perspiciatis ullam consectetur nesciunt iste quos mollitia?
+            </p>
+            <a class="btn btn-success btn-block" href="/login" style="font-size: 20px;">Coba Sekarang</a><br>
+            Photo by <strong>Tembela Bohle</strong> from Pexels
+        </div>
+    </div>
+    @endsection
 </body>
 </html>
