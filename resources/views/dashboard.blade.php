6694df2d77d0f2d344e7f0492dbffe7fbc257d38
yohanesrscvt <yohanesrajabasasitorus@hotmail.com>
2021-06-21 00:28:00 +0700

    benerin navbar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,46 +1,74 @@
+@extends('master')
+@section('title','Home')
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
 </head>
 <body>
+    <!-- navbar -->
+    @section('navbar')
+    <nav>
+        <div class="navigation-bar">
+            <div class="item-left">
+                <div class="home-nav">
+                    <a href="" style="color:green;">Home</a>
+                </div>
+
+                @if($role == "Customer") 
+                <div class="kategori-nav">
+                    <a href="">Kategori</a>
+                </div>
+                @endif
+
+                <div class="about-nav">
+                    <a class="about-nav" href="">About</a>
+                </div>
+                <div class="FAQ-nav">
+                    <a class="FAQ-nav" href="">FAQ</a>
+                </div>
+            </div>
+
+            <div class="item-right">
+                <a href="/profile/main">
+                    <div class="profile">
+                    @if($role == "Customer") 
+                        @foreach($CustomerData as $c)
+                        <img src="{{asset('storage/' . $c->CustomerPicturePath)}}" alt="profile">
+                        @endforeach
+                    @elseif($role == "Agent")
+                        @foreach($AgentData as $a)
+                            <img src="{{asset('storage/' . $a->AgentPicturePath)}}" alt="profile">
+                        @endforeach
+                    @endif
+                    </div>
+                </a>
+
+                <!-- change role -->
+                @if($role == "Customer") 
+                <select name="role" id="role" onchange="location = this.value;" autofocus>
+                    <option value="" selected>Customer</option>
+                    <option value="/set_agent">Agent</option>
+                </select>
+                @elseif($role == "Agent")
+                <select name="role" id="role" onchange="location = this.value;" autofocus>
+                    <option value="/set_customer">Customer</option>
+                    <option value="" selected>Agent</option>
+                </select>
+                @endif
+            </div>
+        </div>
+    </nav>
+    @endsection
+
+    @section('content')
     <!-- customer -->
     @if($role == "Customer") 
-        <h1>This is Customer Dashboard</h1><br>
-
-        @foreach($CustomerData as $c)
-            <img src='{{$c->CustomerPicturePath}}' alt="User Profile Image"><br>
-            <p>Saldo: {{$c->CustomerSaldo}}</p><br>
-        @endforeach
-
-        <!-- change role -->
-        <select name="role" id="role" onchange="location = this.value;" autofocus>
-            <option value="" selected>Customer</option>
-            <option value="/set_agent">Agent</option>
-        </select><br>
-
-        <a href="/profile/main">My Account</a>
 
     <!-- agent -->
     @elseif($role == "Agent")
-        <h1>This is Agent Dashboard</h1><br>
-        
-        @foreach($AgentData as $a)
-            <img src='{{$a->AgentPicturePath}}' alt="User Profile Image"><br>
-            <p>Saldo: {{$a->AgentSaldo}}</p><br>
-        @endforeach
-
-        <!-- change role -->
-        <select name="role" id="role" onchange="location = this.value;" autofocus>
-            <option value="/set_customer">Customer</option>
-            <option value="" selected>Agent</option>
-        </select><br>
-        <!-- end change role -->
-
-        <a href="/profile/main">My Account</a><br>
         <a href="/dashboard/agent/add">Add Product</a>
         <hr>
         <br>
@@ -59,5 +87,6 @@
         @endforeach
         <!-- end product view -->
     @endif
+    @endsection
 </body>
 </html>
--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -10,41 +10,13 @@
 </head>
 <body>
     @foreach($UserData as $ud)
-    <!-- navbar -->
-    @section('navbar')
-    <nav>
-        <div class="navigation-bar">
-            <div class="item-left">
-                <div class="home-nav">
-                    <a href="">Home</a>
-                </div>
-                <div class="kategori-nav">
-                    <a href="">Kategori</a>
-                </div>
-                <div class="about-nav">
-                    <a class="about-nav" href="">About</a>
-                </div>
-                <div class="FAQ-nav">
-                    <a class="FAQ-nav" href="">FAQ</a>
-                </div>
-            </div>
-
-            <!-- <div class="item-right">
-                <a href="profile.html">
-                    <div class="profile">
-                        <img src="../Images/batik.jpg" alt="profile" width="100px" height="100px">
-                    </div>
-                </a>
-            </div> -->
-        </div>
-    </nav>
-    @endsection
     <!-- content -->
     @section('content')
     <div class="container">
         <a href="/profile/back" class="btn btn-outline-danger">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-backspace-fill" viewBox="0 0 16 16">
-        <path d="M15.683 3a2 2 0 0 0-2-2h-7.08a2 2 0 0 0-1.519.698L.241 7.35a1 1 0 0 0 0 1.302l4.843 5.65A2 2 0 0 0 6.603 15h7.08a2 2 0 0 0 2-2V3zM5.829 5.854a.5.5 0 1 1 .707-.708l2.147 2.147 2.146-2.147a.5.5 0 1 1 .707.708L9.39 8l2.146 2.146a.5.5 0 0 1-.707.708L8.683 8.707l-2.147 2.147a.5.5 0 0 1-.707-.708L7.976 8 5.829 5.854z"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+        <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
         </svg>
         </a>
         <h1>Profile</h1>
@@ -63,6 +35,7 @@
                         <h4 style="color:white;"><span class="badge bg-secondary">Saldo anda : {{$ud->CustomerSaldo}}</span></h4>
                         <a href="" class="btn btn-success"> TopUp Saldo</>
                         <a href="" class="btn btn-warning" style="color:white; margin-left:5px;">Riwayat Transaksi</a>
+                        <a href="" class="btn btn-dark" style="color:white; margin-left:5px;">Riwayat Penjualan</a>
                     </div>
                 </div>
                 <div class="saldo">
